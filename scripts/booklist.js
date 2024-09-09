e042ac1e7c79f69a5dfb5df3b15860c6dda8ab4a
shuguang2000 <92711447+shuguang2000@users.noreply.github.com>
2024-09-09 20:52:27 +0800

    Add files via upload

--- scripts/booklist.js
+++ scripts/booklist.js
@@ -1,76 +1,17 @@
-hexo.extend.helper.register('my_list_categories', function(categories){
-    
-    if (!categories || !categories.length) return '';
-  
-    const prepareQuery = parent => {
-      const query = {};
-  
-      if (parent) {
-        query.parent = parent;
-      } else {
-        query.parent = {$exists: false};
-      }
-  
-      return categories.find(query).sort('name', 1).filter(cat => cat.length);
-    };
-  
-    const hierarchicalList = (level, parent) => {
-      
-      if(level > 1) return '';
-      let result = '';
-  
-      prepareQuery(parent).forEach((cat, i) => {
-  
-        let child;
-        child = hierarchicalList(level + 1, cat._id);
-  
-        if (child) {
-          result += `<h2 class="archive-main-category">${cat.name}</h2>`;
-        } else {
-          result += `<div class="subtitle-item"><div class="subtitle-info">${cat.name}</div></div>`;
-  
-          //sort by date
-          var posts = {};
-          for(var n = 0; n < cat.posts.length; n++)
-          {
-            var post1 = cat.posts.data[n];
-            var date1 = this.date(post1.date, "YYYY-MM-DD hh:mm:ss") + post1.title;
-            posts[date1] = post1;
-          }
-          function objKeySort(obj) {
-            var newkey = Object.keys(obj).sort();
-            var newObj = {};
-            for (var i = 0; i < newkey.length; i++) {
-                newObj[newkey[i]] = obj[newkey[i]];
-            }
-            return newObj;
-          }
-          posts = objKeySort(posts)
-          var posts_keys = Object.keys(posts);
-  
-          for(var k = 0; k < posts_keys.length; k++)
-          {
-            var post2 = posts[posts_keys[k]];
-            result += `<div class="post-item">`;
-            result += `<div class="post-info">${this.date(post2.date, "YYYY-MM-DD")}</div>`;
-            result += `<a href="${this.url_for(post2.path)}" class="post-title-link" target="_blank">`;
-            result += `${post2.title}</a></div>`;
-  
-            if(k == (cat.posts.length-1))
-            {
-              result += `<p> </p>`;
-            }
-          }
-        }
-  
-        if (child) {
-          result += `${child}`;
-        }
-      });
-  
-      return result;
-    };
-  
-    return `<ul class="post-archive">${hierarchicalList(0)}</ul>`;
-  
-  });
+document.addEventListener('DOMContentLoaded', function() {
+  var links = document.createElement('div');
+  links.id = 'links';
+  document.body.appendChild(links);
+
+  var links_content = document.createElement('div');
+  links_content.className = 'links-content';
+  links.appendChild(links_content);
+
+  var link_navigation = document.createElement('div');
+  links_content.className = 'link-navigation';
+  link_navigation.innerHTML = "test";
+  links_content.appendChild(link_navigation);
+
+
+
+}, false);
